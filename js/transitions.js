3b7403365cbf00080aa9b146ac1bc1eccb2b387f
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-08-02 09:05:19 -0400

    basic house keeping.

--- js/transitions.js
+++ js/transitions.js
@@ -1,12 +1,1 @@
 
-function showModal() {
-  $('.projects-modal').addClass('show');
-}
-
-function hideModal() {
-  $('.projects-modal').removeClass('show');
-}
-
-
-$('.projects-open-button').on('click', showModal);
-$('.projects-modal').on('click', hideModal);
