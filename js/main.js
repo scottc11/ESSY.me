6134bf0dc295d918f60da582a78e80002699db97
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-10-07 14:12:41 -0400

    added code for button transitions but did not implement them.

--- js/main.js
+++ js/main.js
@@ -4,30 +4,47 @@
 // %100 opaque, display the page content.
 
 
-function onReady() {
+// fade out the modal when background image is loaded.
+function backgroundReady() {
 
   $('#loader').attr('style', 'display: none;');
   $('#modal').addClass('fade-out');
 
-  $('#modal').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-    function() {
-      $('#page').attr('style', 'display: block;');
-    });
+  $('#modal').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', transitionBody());
+}
+
+$( window ).ready(
+  function() {
+
+    console.log("document READY");
+
+  }
+);
+
+function transitionBody() {
+  console.log("transitionBody() START");
+  $('#page').attr('style', 'display: block;');
+
+  
+
+  console.log("transitionBody() END");
 }
 
 
 // Background Image Loading -------------------
 
-  var backgroundImage = new Image();
+var backgroundImage = new Image();
 
-  var backgroundSource = "../img/background_image_alt.jpg";
+var backgroundSource = "../img/background_image_alt.jpg";
 
-  backgroundImage.onload = function() {
-    $('body').attr("style", "background-image: url(" + backgroundSource + ")");
-    setTimeout(function() { onReady() }, 1000);
-  };
+backgroundImage.onload = function() {
+  console.log("setting background");
+  $('body').attr("style", "background-image: url(" + backgroundSource + ")");
+  // setTimeout(function() { backgroundReady() }, 1000);
+  backgroundReady();
+};
 
-  backgroundImage.src = backgroundSource;
+backgroundImage.src = backgroundSource;
 
 
 // Background Image Loading -------------------
